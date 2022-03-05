90ae2f187ec5b8d197d48a0e5aaaadad1287e589
SquidScopes <overenchanted.gaming@gmail.com>
2022-03-05 16:41:18 +0000

    Fixed tab inconsistancy

--- cogs/ender_listen.py
+++ cogs/ender_listen.py
@@ -2,39 +2,43 @@ import discord
 from discord.ext import commands
 from replit import db
 
+
 class Ed(commands.Cog):
     def __init__(self, bot):
-      self.bot = bot
+        self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, msg):
-      def check(m):
-        return m.channel == msg.channel and m.author.id == 878007103460089886
+        def check(m):
+            return m.channel == msg.channel and m.author.id == 878007103460089886
 
-      msg.content = msg.content.lower()
-      user = msg.author
-  
-      if msg.content == "m!ed" or msg.content == "m!enderdragon" or msg.content == "m!bf" or msg.content == "m!bossfight":
+        msg.content = msg.content.lower()
+        user = msg.author
 
-        try:
-          msg2 = await self.bot.wait_for("message", check=check, timeout=20.0)
-        except:
-          pass
-          
-        try:
-          embed = msg2.embeds[0].to_dict()
-          damage = embed["description"].split("\n")[0].split(" ")[2]
-          db[str(user.id)]["stats"]["damage"] = damage
-          return
-        except:
-          pass
-        
-        try:
-          embed = msg2.embeds[0].to_dict()
-          damage = embed["description"].split(" ")[2]
-          db[str(user.id)]["stats"]["damage"] = damage
-        except:
-          pass
-        
-def setup(bot): 
-    bot.add_cog(Ed(bot)) # register the cog 
+        if msg.content == "m!ed" or msg.content == "m!enderdragon" or msg.content == "m!bf" or msg.content == "m!bossfight":
+
+            try:
+                msg2 = await self.bot.wait_for("message",
+                                               check=check,
+                                               timeout=20.0)
+            except:
+                pass
+
+            try:
+                embed = msg2.embeds[0].to_dict()
+                damage = embed["description"].split("\n")[0].split(" ")[2]
+                db[str(user.id)]["stats"]["damage"] = damage
+                return
+            except:
+                pass
+
+            try:
+                embed = msg2.embeds[0].to_dict()
+                damage = embed["description"].split(" ")[2]
+                db[str(user.id)]["stats"]["damage"] = damage
+            except:
+                pass
+
+
+def setup(bot):
+    bot.add_cog(Ed(bot))  # register the cog
