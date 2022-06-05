9684bc054a0d89d3ddc6bbae0cce9f5806c9dfd3
JustaSqu1d <overenchanted.gaming@gmail.com>
2022-06-05 05:01:02 +0000

    Fixed "No such file or directory"

--- bot/main.py
+++ bot/main.py
@@ -46,7 +46,7 @@ event = bot.create_group("event", "Event settings")
 
 stat_start = 1647338400
 
-for filename in os.listdir("bot/cogs"):
+for filename in os.listdir("cogs/"):
     if filename.endswith(".py"):
         bot.load_extension(f"cogs.{filename[:-3]}")
         
